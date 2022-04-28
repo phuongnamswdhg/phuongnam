f68daef6079bb65185db6656717cf61f36db88bc
phuongnamswdhg <92508760+phuongnamswdhg@users.noreply.github.com>
2022-04-28 17:14:44 +0700

    Create CustomerDao.java

---
+++ CustomerDao.java
@@ -0,0 +1,15 @@
+package jdbcpracticaltest.model;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import jdbcpracticaltest.entity.Customer;
+
+public interface CustomerDao {
+    public void addNewCustomer(Customer customer) throws SQLException, ClassNotFoundException;
+
+    public ArrayList<Customer> findCustomerByName(String name) throws ClassNotFoundException, SQLException;
+
+    public ArrayList<Customer> showAllCustomers() throws ClassNotFoundException, SQLException;
+}
+© 2022 GitHub, Inc.
